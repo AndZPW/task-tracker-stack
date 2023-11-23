2151a1da133cdfb1c2646c256a34dedd149d3633
AndZPW <100237143+AndZPW@users.noreply.github.com>
2023-11-23 21:17:07 +0200

    crearte email-creator-service

---
+++ email-creator/src/main/java/dev/andzwp/emailcreator/EmailCreatorApplication.java
@@ -0,0 +1,13 @@
+package dev.andzwp.emailcreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailCreatorApplication.class, args);
+    }
+
+}
---
+++ email-creator/src/test/java/dev/andzwp/emailcreator/EmailCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andzwp.emailcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
