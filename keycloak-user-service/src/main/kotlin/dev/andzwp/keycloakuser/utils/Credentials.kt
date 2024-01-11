7b6bdf2136475f7aa635d5fc3404fd6d99a6bdf3
AndZPW <100237143+AndZPW@users.noreply.github.com>
2024-01-11 16:46:51 +0200

    create Credentials and UserService

--- keycloak-user-service/src/main/kotlin/dev/andzwp/keycloakuser/utils/Credentials.kt
+++ keycloak-user-service/src/main/kotlin/dev/andzwp/keycloakuser/utils/Credentials.kt
@@ -1,4 +1,15 @@
 package dev.andzwp.keycloakuser.utils
 
+import org.keycloak.representations.idm.CredentialRepresentation
+import org.springframework.stereotype.Component
+
+@Component
 class Credentials {
+    fun createCredentialsPassword(password: String): CredentialRepresentation {
+        val passwordCredentials = CredentialRepresentation()
+        passwordCredentials.isTemporary = false
+        passwordCredentials.type = CredentialRepresentation.PASSWORD
+        passwordCredentials.value = password
+        return passwordCredentials
+    }
 }
